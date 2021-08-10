b68eb2239d0d3d0b632a590a9a81d4024e77475c
JordanAmaralVicente <jordanitalovicente@gmail.com>
2021-08-10 14:09:02 -0300

    fix: fix date verify

--- main.py
+++ main.py
@@ -6,33 +6,34 @@ import pandas as pd
 from sqlalchemy.orm import sessionmaker
 import requests
 import json
-from datetime import datetime
 import datetime
 import sqlite3
+import time
 
 DATABASE_LOCATION = 'sqlite:///my_played_tracks.sqlite'
 USER_ID = 'jordanitalovicente'
-TOKEN = 'BQAUE9vGvHHNbUuIonNbmNoVElXya2reuHRQSYyFMPOPvpWXcyy1dmT7ADCAAhHcKrsggE6oCHAGVLxDCIA0nNel6LhZVUaNeUl3yXngNQB56ZB6gK7qJFvxIZmSziwUBT2v900ikYzCwHzpk_xyknMdFmd4EKFjvx2x5jU'
-
+TOKEN = 'BQBOfi58nLwH7n7QggfDFvol8pIMatq4pwAgWABcp_bjibO1Mqbs5U2sRTx_4EyFfgG50fFmS8Dbw10998WOv6JOu3iJcv6WPjNUL30prDpHv2jr0zA2mjw5sXCpgzk3HsDEGQG_pI_1H7Qx_5Flf5dDa4k0_iglWzhdRzA'
 def check_if_valid_date(df: pd.DataFrame) -> bool:
     if df.empty:
         print('No songs downloaded. Finishing execution.')
         return False
-    
+
     if pd.Series(df['played_at']).is_unique:
         pass
     else:
         raise Exception('Primary Key Check is violated')
-    
+
     if df.isnull().values.any():
         raise Exception('Null value found')
-    
-    yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
-    yesterday = yesterday.replace(hour=0, minute=0, second=0, microsecond=0)
 
-    timestamps = df['timestamp'].tolist()
+    yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
+    yesterday_unix_timestamp = int(yesterday.timestamp())
+
+    timestamps = df['played_at'].tolist()
     for timestamp in timestamps:
-        if datetime.datetime.strptime(timestamp, '%Y-%m-%d') != yesterday:
+        datetimeObj = datetime.datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S.%fZ')
+        timestamp = time.mktime(datetimeObj.timetuple())
+        if timestamp < yesterday_unix_timestamp:
             raise Exception('At least one of the returned songs does not come from within the last 24 hours')
 
     return True
@@ -50,7 +51,7 @@ if __name__ == '__main__':
     yesterday_unix_timestamp = int(yesterday.timestamp()) * 1000
 
     r = requests.get('https://api.spotify.com/v1/me/player/recently-played?after={time}'.format(time=yesterday_unix_timestamp), headers = headers)
-    
+
     data = r.json()
 
     song_names = []
@@ -73,5 +74,6 @@ if __name__ == '__main__':
 
     song_df = pd.DataFrame(song_dict, columns= ['song_names', 'artist_names', 'played_at', 'timestamp'])
 
+    print(song_df)
     if check_if_valid_date(song_df):
         print('Data valid, proceed to load stage')
